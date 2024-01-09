2dff87806ea3df4da2fc8bbc5bc76fd6bff91f50
HugoRbrt <roberthugo@icloud.com>
2024-01-09 11:43:18 +0100

    conf.py changed

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 NB_ID = "test-notebook"  # This will be the name which appears on Kaggle.
 GIT_USER = "HugoRbrt"  # Your git user name
-GIT_REPO = "mva_pepites"  # Your current git repo
+GIT_REPO = "altegrad_project"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
 KAGGLE_DATASET_LIST = []
