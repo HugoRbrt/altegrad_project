aa058e6b474c1cb50a001730745f2f2d31b0001c
HugoRbrt <roberthugo@icloud.com>
2024-01-09 16:13:02 +0100

    add dataset

--- configuration.py
+++ configuration.py
@@ -2,4 +2,4 @@ NB_ID = "test-notebook"  # This will be the name which appears on Kaggle.
 GIT_USER = "HugoRbrt"  # Your git user name
 GIT_REPO = "altegrad_project"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
-KAGGLE_DATASET_LIST = []
+KAGGLE_DATASET_LIST = ['matteomarngo/datanlp']
