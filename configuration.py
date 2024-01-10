5ebf2dbc1024cc34bbc373426df22ddff485d0ee
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-10 17:00:09 +0100

    quick fix

--- configuration.py
+++ configuration.py
@@ -1,12 +1,12 @@
 NB_ID = "notebook-altegrad"  # This will be the name which appears on Kaggle.
-GIT_USER = "b-ptiste"  # Your git user name
+GIT_USER = "HugoRbrt"  # Your git user name
 GIT_REPO = "altegrad_project"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
 KAGGLE_DATASET_LIST = ['hugorbrt/nlplsv3']
 CFG_EXPERIMENTS = {
     0:{  
-        'who': GIT_USER,
-        'name_exp': "Baseline",
+        'who': 'baptiste',
+        'name_exp': "test_bpt",
         'nb_epochs': 5,
         'batch_size': 32,
         'learning_rate': 2e-5,
