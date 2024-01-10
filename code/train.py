5e4139b1670812a84f33611f7e19145062fc7464
HugoRbrt <roberthugo@icloud.com>
2024-01-10 12:32:13 +0100

    correct name for artifacts

--- code/train.py
+++ code/train.py
@@ -166,11 +166,11 @@ def run_experiment(cfg, cpu=False, no_wandb=False):
     
     if not no_wandb:
         
-        submission_artifact = wandb.Artifact('submission:'+str(uuid.uuid1()).replace("-",""), type='csv')
+        submission_artifact = wandb.Artifact('submission'+str(uuid.uuid1()).replace("-",""), type='csv')
         submission_artifact.add_file('submission.csv')
         wandb.log_artifact(submission_artifact)
         
-        model_artifact = wandb.Artifact('model:'+str(uuid.uuid1()).replace("-",""), type='model')
+        model_artifact = wandb.Artifact('model'+str(uuid.uuid1()).replace("-",""), type='model')
         model_artifact.add_file(save_path)
         wandb.log_artifact(model_artifact)
         wandb.finish()
