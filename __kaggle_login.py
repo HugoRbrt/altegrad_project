d8205b85950e35aa3f14202ed24955a4fd0e8af8
HugoRbrt <105380296+HugoRbrt@users.noreply.github.com>
2024-01-10 16:32:31 +0100

    Update __kaggle_login.py

--- __kaggle_login.py
+++ __kaggle_login.py
@@ -1,10 +1,6 @@
 kaggle_users = {
-    "hugo_icloud": {
-        "username":"hugorbrt",
-        "key":"1d7a8f670d8a772667539a64f4263cba"
-    },
-    "hugo_gmail": {
-        "username": "hugorbrt92",
-        "key": "4e77414ff73f1943241e4769692834d2"
+    "user0": {
+        "username":"fill_me",
+        "key":"fill_me"
     },
 }
