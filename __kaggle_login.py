cc8c3dbc2dd3325e198bf6410e55ba084ef572df
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-10 16:44:52 +0100

    Change config

--- __kaggle_login.py
+++ __kaggle_login.py
@@ -1,10 +1,10 @@
 kaggle_users = {
-    "hugo_icloud": {
-        "username":"hugorbrt",
-        "key":"1d7a8f670d8a772667539a64f4263cba"
+    "bapt_1": {
+        "username":"baptistecallard",
+        "key":"3402005399f441481fd1e7502316d267"
     },
-    "hugo_gmail": {
-        "username": "hugorbrt92",
-        "key": "4e77414ff73f1943241e4769692834d2"
-    },
+#     "hugo_gmail": {
+#         "username": "hugorbrt92",
+#         "key": "4e77414ff73f1943241e4769692834d2"
+#     },
 }
--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 NB_ID = "notebook-altegrad"  # This will be the name which appears on Kaggle.
-GIT_USER = "HugoRbrt"  # Your git user name
+GIT_USER = "b-ptiste"  # Your git user name
 GIT_REPO = "altegrad_project"  # Your current git repo
 # Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
 KAGGLE_DATASET_LIST = ['hugorbrt/nlplsv3']
@@ -17,17 +17,17 @@ CFG_EXPERIMENTS = {
         'graph_hidden_channels': 300,
         'comment': '',
     },
-    1:{  
-        'who': GIT_USER,
-        'name_exp': "Baseline + high learning_rate",
-        'nb_epochs': 5,
-        'batch_size': 32,
-        'learning_rate': 1e-4,
-        'model_name': 'distilbert-base-uncased',
-        'num_node_features': 300,
-        'nout':  768,
-        'nhid': 300,
-        'graph_hidden_channels': 300,
-        'comment': '',
-    },
+    # 1:{  
+    #     'who': GIT_USER,
+    #     'name_exp': "Baseline + high learning_rate",
+    #     'nb_epochs': 5,
+    #     'batch_size': 32,
+    #     'learning_rate': 1e-4,
+    #     'model_name': 'distilbert-base-uncased',
+    #     'num_node_features': 300,
+    #     'nout':  768,
+    #     'nhid': 300,
+    #     'graph_hidden_channels': 300,
+    #     'comment': '',
+    # },
 }
