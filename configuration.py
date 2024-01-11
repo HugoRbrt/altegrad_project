16e8120f6cb6e0754b20669981210268e137b032
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-11 12:33:22 +0100

    attention pooling

--- configuration.py
+++ configuration.py
@@ -6,7 +6,7 @@ KAGGLE_DATASET_LIST = ['hugorbrt/nlplsv3']
 CFG_EXPERIMENTS = {
     0:{  
         'who': 'baptiste',
-        'name_exp': "maxpooling + GATv2Conv (3) + AttentionPooling",
+        'name_exp': "maxpooling + GATv2Conv (3) + Skip Connection",
         'scheduler': 'CosineAnnealingLR',
         'nb_epochs': 15,
         'batch_size': 20,
