b3c65cff2dea422e1d518b0d3a7ad900c763ec1a
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-11 08:33:14 +0100

    Try to efficiently manage memory

--- code/train.py
+++ code/train.py
@@ -48,11 +48,15 @@ def run_experiment(cfg, cpu=False, no_wandb=False):
     gt = np.load("/kaggle/input/nlplsv3/kaggle/working/token_embedding_dict.npy", allow_pickle=True)[()]
     val_dataset = GraphTextDataset(root='/kaggle/input/nlplsv3/kaggle/working/', gt=gt, split='val', tokenizer=tokenizer)
     train_dataset = GraphTextDataset(root='/kaggle/input/nlplsv3/kaggle/working/', gt=gt, split='train', tokenizer=tokenizer)
-
+    test_cids_dataset = GraphDataset(root='/kaggle/input/nlplsv3/kaggle/working/', gt=gt, split='test_cids')
+    test_text_dataset = TextDataset(file_path='/kaggle/input/nlplsv3/kaggle/working/test_text.txt', tokenizer=tokenizer)
+    
     device = "cpu" if cpu else ("cuda" if torch.cuda.is_available() else "cpu")
 
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_text_loader = TorchDataLoader(test_text_dataset, batch_size=batch_size // 4, shuffle=False)
+
 
     model = Model(model_name=model_name, num_node_features=cfg['num_node_features'], nout=cfg['nout'], nhid=cfg['nhid'], graph_hidden_channels=cfg['graph_hidden_channels'], heads=cfg['heads']) # nout = bert model hidden dim
     model.to(device)
@@ -103,18 +107,19 @@ def run_experiment(cfg, cpu=False, no_wandb=False):
                 loss = 0 
         scheduler.step()
         model.eval()       
-        val_loss = 0        
-        for batch in val_loader:
-            input_ids = batch.input_ids
-            batch.pop('input_ids')
-            attention_mask = batch.attention_mask
-            batch.pop('attention_mask')
-            graph_batch = batch
-            x_graph, x_text = model(graph_batch.to(device), 
-                                    input_ids.to(device), 
-                                    attention_mask.to(device))
-            current_loss = contrastive_loss(x_graph, x_text)   
-            val_loss += current_loss.item()
+        val_loss = 0
+        with torch.no_grad():    
+            for batch in val_loader:
+                input_ids = batch.input_ids
+                batch.pop('input_ids')
+                attention_mask = batch.attention_mask
+                batch.pop('attention_mask')
+                graph_batch = batch
+                x_graph, x_text = model(graph_batch.to(device), 
+                                        input_ids.to(device), 
+                                        attention_mask.to(device))
+                current_loss = contrastive_loss(x_graph, x_text)   
+                val_loss += current_loss.item()
         best_validation_loss = min(best_validation_loss, val_loss)
         print('-----EPOCH'+str(i+1)+'----- done.  Validation loss: ', str(val_loss/len(val_loader)) )
         if not no_wandb:
@@ -144,24 +149,20 @@ def run_experiment(cfg, cpu=False, no_wandb=False):
     graph_model = model.get_graph_encoder()
     text_model = model.get_text_encoder()
 
-    test_cids_dataset = GraphDataset(root='/kaggle/input/nlplsv3/kaggle/working/', gt=gt, split='test_cids')
-    test_text_dataset = TextDataset(file_path='/kaggle/input/nlplsv3/kaggle/working/test_text.txt', tokenizer=tokenizer)
-
     idx_to_cid = test_cids_dataset.get_idx_to_cid()
 
     test_loader = DataLoader(test_cids_dataset, batch_size=batch_size, shuffle=False)
+    with torch.no_grad():
+        graph_embeddings = []
+        for batch in test_loader:
+            for output in graph_model(batch.to(device)):
+                graph_embeddings.append(output.tolist())
 
-    graph_embeddings = []
-    for batch in test_loader:
-        for output in graph_model(batch.to(device)):
-            graph_embeddings.append(output.tolist())
-
-    test_text_loader = TorchDataLoader(test_text_dataset, batch_size=batch_size, shuffle=False)
-    text_embeddings = []
-    for batch in test_text_loader:
-        for output in text_model(batch['input_ids'].to(device), 
-                                attention_mask=batch['attention_mask'].to(device)):
-            text_embeddings.append(output.tolist())
+        text_embeddings = []
+        for batch in test_text_loader:
+            for output in text_model(batch['input_ids'].to(device), 
+                                    attention_mask=batch['attention_mask'].to(device)):
+                text_embeddings.append(output.tolist())
 
     similarity = cosine_similarity(text_embeddings, graph_embeddings)
 
