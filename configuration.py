f460a7e85d5e6e691b4d8db2b341910adb128321
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-12 18:27:17 +0100

    increase size

--- configuration.py
+++ configuration.py
@@ -26,7 +26,7 @@ CFG_EXPERIMENTS = {
         'nb_epochs': 15,
         'batch_size': 24,
         'learning_rate': 2e-5,
-        'model_name': ' distilbert-base-uncased',
+        'model_name': 'distilbert-base-uncased',
         'num_node_features': 600,
         'nout':  768,
         'nhid': 600,
