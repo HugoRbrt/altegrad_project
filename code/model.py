8bc29d618b312ea4a9147223c9fdbf1358cc1ee4
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-12 19:19:13 +0100

    Add DeepSet

--- code/model.py
+++ code/model.py
@@ -5,45 +5,54 @@ from torch_geometric.nn import GCNConv, MFConv, GATv2Conv, SuperGATConv
 from torch_geometric.nn import global_mean_pool, global_max_pool
 from transformers import AutoModel
 
+
+class DeepSets(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hidden_dim):
+        super(DeepSets, self).__init__()
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.fc1 = nn.Linear(embedding_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        
+        ############## Task 3
+    
+        ##################
+        # your code here #
+        ##################
+        x = self.embedding(x)
+        x = self.tanh(self.fc1(x))
+        x = torch.sum(x, dim=1)
+        x = self.fc2(x)       
+                
+        return x.squeeze()
+    
+    
 class GraphEncoder(nn.Module):
-    def __init__(self, num_node_features, nout, nhid, graph_hidden_channels, heads):
+    def __init__(self, nout, nhid):
         super(GraphEncoder, self).__init__()
         self.nhid = nhid
         self.nout = nout
         self.relu = nn.ReLU()
-        self.ln = nn.LayerNorm((nout))
-        self.conv1 = GATv2Conv(num_node_features, graph_hidden_channels, heads=heads)
-        self.skip_1 = nn.Linear(num_node_features, graph_hidden_channels * heads)
-        self.conv2 = GATv2Conv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
-        self.skip_2 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
-        self.conv3 = GATv2Conv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
-        self.skip_3 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
+        self.mol_hidden1 = nn.Linear(nout, nhid)
+        self.mol_hidden2 = nn.Linear(nhid, nhid)
+        self.mol_hidden3 = nn.Linear(nhid, nout)
 
-        self.mol_hidden1 = nn.Linear(graph_hidden_channels * heads, nhid)
-        self.mol_hidden2 = nn.Linear(nhid, nout)
-
+        self.ln1 = nn.LayerNorm((nout))
+        
     def forward(self, graph_batch):
         x = graph_batch.x
         edge_index = graph_batch.edge_index
         batch = graph_batch.batch
-        x1 = self.conv1(x, edge_index)
-        skip_x = self.skip_1(x)  # Prepare skip connection
-        x = skip_x + x1  # Apply skip connection
-        x = self.relu(x)
         
-        x2 = self.conv2(x, edge_index)
-        skip_x = self.skip_2(x)  # Prepare skip connection
-        x = skip_x + x2  # Apply skip connection
-        x = self.relu(x)
+        x = self.relu(self.mol_hidden1(x))
+        x = self.relu(self.mol_hidden2(x))
+        x = torch.sum(x, dim=1)
+        x = self.mol_hidden3(x)
         
-        x3 = self.conv3(x, edge_index)
-        skip_x = self.skip_3(x)  # Prepare skip connection
-        x = skip_x + x3  # Apply skip connection
-        x = self.relu(x)
+        x = self.ln1(x)
         
-        x = global_max_pool(x, batch)
-        x = self.mol_hidden1(x).relu()
-        x = self.mol_hidden2(x)
         return x
 
 class AttentionPooling(nn.Module):
@@ -81,7 +90,7 @@ class TextEncoder(nn.Module):
 class Model(nn.Module):
     def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels, heads):
         super(Model, self).__init__()
-        self.graph_encoder = GraphEncoder(num_node_features, nout, nhid, graph_hidden_channels, heads)
+        self.graph_encoder = GraphEncoder(nout, nhid)
         self.text_encoder = TextEncoder(model_name, nout)
         
     def forward(self, graph_batch, input_ids, attention_mask):
