f37884b7c2e8e328e477dd1ca2de7dec4b7c00c2
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-14 10:30:38 +0100

    GATConv(3)

--- code/train.py
+++ code/train.py
@@ -66,7 +66,7 @@ def run_experiment(cfg, cpu=False, no_wandb=False):
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate,
                                     betas=(0.9, 0.999),
                                     weight_decay=0.01)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
 
     epoch = 0
     loss = 0
