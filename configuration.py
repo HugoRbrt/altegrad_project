9aeb7c2a5dc87136908a3ccd7954a38a7c53f92f
HugoRbrt <roberthugo@icloud.com>
2024-01-17 18:47:43 +0100

    batch size

--- configuration.py
+++ configuration.py
@@ -8,7 +8,7 @@ CFG_EXPERIMENTS = {
         'who': GIT_USER,
         'name_exp': "MLP dropout nhid 600",
         'nb_epochs': 60,
-        'batch_size': 48,
+        'batch_size': 96,
         'learning_rate': 3e-5,
         'model_name': 'distilbert-base-uncased',
         'num_node_features': 300,
