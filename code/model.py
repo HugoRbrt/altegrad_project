b8dcb21c878f84959cc2eca626e839605a360c6a
HugoRbrt <roberthugo@icloud.com>
2024-01-22 16:37:07 +0100

    test time textEncoder

--- code/model.py
+++ code/model.py
@@ -213,15 +213,15 @@ class Model(nn.Module):
     def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels, heads):
         super(Model, self).__init__()
         #self.graph_encoder = GINConModel(num_node_features, nout, nhid)
-        self.graph_encoder = MLPModel(num_node_features, nout, nhid)
+        #self.graph_encoder = MLPModel(num_node_features, nout, nhid)
         #self.graph_encoder = GatConv(num_node_features, nout, nhid, graph_hidden_channels, heads)
         self.text_encoder = TextEncoder(model_name, nout)
         
     def forward(self, graph_batch, input_ids, attention_mask):
-        graph_encoded = self.graph_encoder(graph_batch)
+        #graph_encoded = self.graph_encoder(graph_batch)
         text_encoded = self.text_encoder(input_ids, attention_mask)
         
-        return graph_encoded, text_encoded
+        return text_encoded, text_encoded
     
     def get_text_encoder(self):
         return self.text_encoder
