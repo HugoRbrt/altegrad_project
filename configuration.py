ecadfbf22717feb7f12d95d84c8aa771d0178087
MatteoMarengo <71354710+MatteoMarengo@users.noreply.github.com>
2024-01-22 10:30:30 +0100

    out of memory - OUT

--- configuration.py
+++ configuration.py
@@ -1,12 +1,13 @@
 NB_ID = "notebook-altegrad"  # This will be the name which appears on Kaggle.
 GIT_USER = "HugoRbrt"  # Your git user name
 GIT_REPO = "altegrad_project"  # Your current git repo
-# Keep free unless you need to acess kaggle datasets. You'll need to modify the remote_training_template.ipynb.
+# Keep free unless you need to acess kaggle datasets. 
+# You'll need to modify the remote_training_template.ipynb.
 KAGGLE_DATASET_LIST = ['hugorbrt/nlplsv3']
 CFG_EXPERIMENTS = {
     0:{  
         'who': 'matteo',
-        'name_exp': "GatConv(4) + Linear(4) + linearscheduler + skip + big training",
+        'name_exp': "GatConv(3) + Linear(3) + linearscheduler + skip + big training",
         'scheduler': 'Linear',
         'nb_epochs': 40,
         'batch_size': 24,
@@ -16,7 +17,7 @@ CFG_EXPERIMENTS = {
         'nout':  768,
         'nhid': 450,
         'graph_hidden_channels': 450,
-        'heads': 25,
+        'heads': 20,
         'comment': 'Guess who is back',
         'T_max': 10,
         'num_warmup_steps': 1000,
