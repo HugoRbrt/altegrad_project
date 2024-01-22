65820d8342590a10c7f58a66015a0093dac10a8c
HugoRbrt <roberthugo@icloud.com>
2024-01-22 16:44:06 +0100

    little mlp

--- code/model.py
+++ code/model.py
@@ -215,18 +215,18 @@ class Model(nn.Module):
     def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels, heads):
         super(Model, self).__init__()
         #self.graph_encoder = GINConModel(num_node_features, nout, nhid)
-        #self.graph_encoder = MLPModel(num_node_features, nout, nhid)
+        self.graph_encoder = MLPModel(num_node_features, nout, nhid)
         #self.graph_encoder = GatConv(num_node_features, nout, nhid, graph_hidden_channels, heads)
         self.text_encoder = TextEncoder(model_name, nout)
         
     def forward(self, graph_batch, input_ids, attention_mask):
-        #graph_encoded = self.graph_encoder(graph_batch)
+        graph_encoded = self.graph_encoder(graph_batch)
         text_encoded = self.text_encoder(input_ids, attention_mask)
         
-        return text_encoded, text_encoded
+        return graph_encoded, text_encoded
     
     def get_text_encoder(self):
         return self.text_encoder
     
     def get_graph_encoder(self):
-        return self.text_encoder
+        return self.graph_encoder
