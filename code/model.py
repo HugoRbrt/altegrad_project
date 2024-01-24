8b78a04c4b1da6fc596b4cdf08945583247aad05
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-24 19:13:03 +0100

    GCONV(3) + scheduler linear + float16

--- code/model.py
+++ code/model.py
@@ -254,8 +254,8 @@ class Model(nn.Module):
     def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels, heads, device_1, device_2):
         super(Model, self).__init__()
         # self.graph_encoder = MLPModel(num_node_features, nout, nhid)
-        # self.graph_encoder = GraphConv(num_node_features, nout, nhid).to(device_1)
-        self.graph_encoder = GraphEncoder_v2(num_node_features, nout, nhid, graph_hidden_channels, heads).to(device_1)
+        self.graph_encoder = GraphConv(num_node_features, nout, nhid).to(device_1)
+        # self.graph_encoder = GraphEncoder_v2(num_node_features, nout, nhid, graph_hidden_channels, heads).to(device_1)
         self.text_encoder = TextEncoder(model_name, nout).to(device_2)
         
     def forward(self, graph_batch, input_ids, attention_mask):
