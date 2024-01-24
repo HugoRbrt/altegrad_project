7edd426a07fcc93c556d9a7855c5d5a0062605b0
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-24 19:50:54 +0100

    GAT(3) + scheduler linear + float16 + freeze 2 first layers

--- code/model.py
+++ code/model.py
@@ -221,12 +221,12 @@ class TextEncoder(nn.Module):
     def __init__(self, model_name, hidden_dim):
         super(TextEncoder, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
-        # for name, param in self.bert.transformer.named_parameters():
-        #     if 'layer.0' in name or 'layer.1' in name:
-        #         param.requires_grad = False
+        for name, param in self.bert.bert.transformer.named_parameters():
+            if 'layer.0' in name or 'layer.1' in name:
+                param.requires_grad = False
                 
-        for param in self.bert.embeddings.parameters():
-            param.requires_grad = False
+        # for param in self.bert.bert.embeddings.parameters():
+        #     param.requires_grad = False
         # self.attentionpooling = AttentionPooling(hidden_dim)
         
     def forward(self, input_ids, attention_mask):
