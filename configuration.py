1b2cf3e0e1fac0c9823b0412ac27751ea32ab8cb
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-24 19:51:55 +0100

    GAT(3) + scheduler linear + float16 + freeze 2 first layers + embedding

--- configuration.py
+++ configuration.py
@@ -21,7 +21,7 @@ CFG_EXPERIMENTS = {
     # },
     0:{  
         'who': 'baptiste',
-        'name_exp': "GAT(3) + scheduler linear + float16 + freeze 2 first layers",
+        'name_exp': "GAT(3) + scheduler linear + float16 + freeze 2 first layers + embedding",
         'scheduler': 'lineair',
         'nb_epochs': 15,
         'batch_size': 100,
