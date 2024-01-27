5d43f7f8d44e638d4527368e3947d98924e792e9
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-27 18:35:15 +0100

    GAT + skip(3) + scheduler linear + float16 + batch 100 + n_layers_text=6 (normal)

--- configuration.py
+++ configuration.py
@@ -7,7 +7,7 @@ CFG_EXPERIMENTS = {
     0:{  
         'who': 'baptiste',
         'learning_rate': 2e-5,
-        'name_exp': "GAT + skip(3) + scheduler linear + float16 + batch 100 + n_layers_text=1",
+        'name_exp': "GAT + skip(3) + scheduler linear + float16 + batch 100 + n_layers_text=6 (normal)",
         'scheduler': 'lineair',
         'nb_epochs': 30,
         'batch_size': 100,
