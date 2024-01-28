a96f20a8c1c24a5804d9dc02310e58dc2a8cee6f
HugoRbrt <roberthugo@icloud.com>
2024-01-28 12:06:03 +0100

    batch 300

--- configuration.py
+++ configuration.py
@@ -10,7 +10,7 @@ CFG_EXPERIMENTS = {
         'name_exp': "MLP",
         'scheduler': 'lineair',
         'nb_epochs': 50,
-        'batch_size': 200,
+        'batch_size': 300,
         'model_name': 'distilbert-base-uncased',
         'num_node_features': 300,
         'nout':  768,
