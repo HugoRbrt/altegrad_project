bb048ac0c55c25c55c0103a61ef6b82ae41752c4
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-01-31 00:15:12 +0100

    BIG GraphEncoder_v2 + BIG SKIP + BIG TRAINING + BIG DUDE + BIG LR=5e-4 + BIG 95 EPOCHS

--- configuration.py
+++ configuration.py
@@ -25,7 +25,7 @@ CFG_EXPERIMENTS = {
         'hidden_dim_text':3072, 
         'dim_text':768,
         'device_1': 'cuda:0',
-        'device_2': 'cuda:1',
+        'device_2': 'cuda:0',
         'with_fast_tokenizer': True,
     },
     # 1:{  
