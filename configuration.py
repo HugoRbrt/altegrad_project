715ff02b8699c01cc787bafc1e3efaa4894901ea
MatteoMarengo <71354710+MatteoMarengo@users.noreply.github.com>
2024-01-30 09:51:42 +0100

    ee"

--- configuration.py
+++ configuration.py
@@ -7,10 +7,10 @@ KAGGLE_DATASET_LIST = ['hugorbrt/nlplsv3']
 CFG_EXPERIMENTS = {
     0:{
         'who': 'matteo',
-        'learning_rate': 1e-6,
-        'name_exp': " 4 SUPERGAT  + BIG Training + 128 batchsize + 2 devices training + FLOAT16 ",
+        'learning_rate': 1e-3,
+        'name_exp': " 4 SUPERGAT  + BIG Training + 96 batchsize + 2 devices training + FLOAT16 + bigLR ",
         'scheduler': 'linear',
-        'nb_epochs': 40,
+        'nb_epochs': 30,
         'batch_size': 96,
         'model_name': 'distilbert-base-uncased',
         'num_node_features': 300,
@@ -18,7 +18,7 @@ CFG_EXPERIMENTS = {
         'nhid': 300,
         'graph_hidden_channels': 300,
         'num_warmup_steps': 1000,
-        'heads': 20,
+        'heads': 25,
         'comment': 'Four SuperGat big batch size',
         'T_max': '',
         'n_heads_text':12, 
