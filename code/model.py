675e633dbc1feaaf15823ed0929b82db9b81e241
MatteoMarengo <71354710+MatteoMarengo@users.noreply.github.com>
2024-01-31 03:00:26 +0100

    why not

--- code/model.py
+++ code/model.py
@@ -263,12 +263,6 @@ class GraphEncoder_SuperGAT(nn.Module):
         self.skip_2 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
         self.conv3 = SuperGATConv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
         self.skip_3 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
-        self.conv4 = SuperGATConv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
-        self.skip_4 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
-        self.conv5 = SuperGATConv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
-        self.skip_5 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
-        self.conv6 = SuperGATConv(graph_hidden_channels * heads, graph_hidden_channels, heads=heads)
-        self.skip_6 = nn.Linear(graph_hidden_channels * heads, graph_hidden_channels * heads)
 
         self.mol_hidden1 = nn.Linear(graph_hidden_channels * heads, nhid)
         self.mol_hidden2 = nn.Linear(nhid, nout)
@@ -290,21 +284,6 @@ class GraphEncoder_SuperGAT(nn.Module):
         x3 = self.conv3(x, edge_index)
         skip_x = self.skip_3(x)  # Prepare skip connection
         x = skip_x + x3  # Apply skip connection
-        x = self.relu(x)
-
-        x4 = self.conv4(x, edge_index)
-        skip_x = self.skip_4(x)  # Prepare skip connection
-        x = skip_x + x4  # Apply skip connection
-        x = self.relu(x)
-
-        x5 = self.conv5(x, edge_index)
-        skip_x = self.skip_5(x)  # Prepare skip connection
-        x = skip_x + x5  # Apply skip connection
-        x = self.relu(x)
-
-        x6 = self.conv6(x, edge_index)
-        skip_x = self.skip_6(x)  # Prepare skip connection
-        x = skip_x + x6  # Apply skip connection
         x = self.relu(x)
         
         x = global_max_pool(x, batch)
@@ -570,7 +549,7 @@ class Model(nn.Module):
         device_1,
         device_2):
         super(Model, self).__init__()
-        self.graph_encoder = GraphEncoder_GATv2(num_node_features, nout, nhid, graph_hidden_channels,heads).to(device_1)
+        self.graph_encoder = GraphEncoder_SuperGAT(num_node_features, nout, nhid, graph_hidden_channels,heads).to(device_1)
         self.text_encoder = TextEncoder(model_name,n_heads_text,n_layers_text,hidden_dim_text, dim_text).to(device_2)
         
     def forward(self, graph_batch, input_ids, attention_mask):
