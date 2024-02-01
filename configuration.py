aeb6cb75a2679ab11f6e164b7d0e7031276390f3
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-02-01 14:25:05 +0100

    last dance

--- configuration.py
+++ configuration.py
@@ -7,9 +7,9 @@ CFG_EXPERIMENTS = {
     0: {  
         'who': 'baptiste',
         'learning_rate': 8e-4,
-        'name_exp': "Last big training skip + graphencoer_GAT",
+        'name_exp': "Last Dance NLPLS training skip + graphencoer_GAT",
         'scheduler': 'linear',
-        'nb_epochs': 30,
+        'nb_epochs': 80,
         'batch_size': 200,
         'model_name': 'distilbert-base-uncased',
         'num_node_features': 300,
