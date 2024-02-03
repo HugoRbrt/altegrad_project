441969feedd6c164eb76e911d7f96b97df214cfe
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-02-04 00:40:45 +0100

    retrain

--- configuration.py
+++ configuration.py
@@ -17,7 +17,7 @@ CFG_EXPERIMENTS = {
         'nhid': 900,
         'graph_hidden_channels': 300,
         'num_warmup_steps': 1000,
-        'heads': 25,
+        'heads': 30,
         'comment': '',
         'T_max': '',
         'n_heads_text':8, 
