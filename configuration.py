3d266afbe00557233120c3e94cd93bb62b9e004e
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2024-02-13 21:42:22 +0100

    GraphGATConv

--- configuration.py
+++ configuration.py
@@ -7,7 +7,7 @@ CFG_EXPERIMENTS = {
     0: {  
         'who': 'baptiste',
         'learning_rate': 8e-4,
-        'name_exp': "MLP + Skip",
+        'name_exp': "GAT + Skip",
         'scheduler': 'linear',
         'nb_epochs':75,
         'batch_size': 200,
